5b17ca3619e6081d7432aa93add16d1564a464b2
evank12 <kurukato2419@gmail.com>
2024-12-10 14:00:19 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-
+// Simulate loading time  
+window.onload = function() {
+    setTimeout(() => {
+        document.getElementById('loading-screen').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }, 2000); // Adjust the timeout as needed (2000ms = 2 seconds)
+}
